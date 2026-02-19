587262b91429e864731c01c1a474b323f4936869
Bhavana V <bhavsv500401@gmail.com>
2026-02-19 14:45:34 +0530

    intial commit

---
+++ fibbonacci.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class fibbonacci {
+  public static void main(String[] args){
+    int n=5;
+    int a=0;
+    int b=1;
+    for(int i=0;i<=n;i++){
+      System.out.println(a+" ");
+      int c=a+b;
+      a=b;
+      b=c;
+    }
+  }
+  
+}
