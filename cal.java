71db1622a96220c0cd9a808f95a283ad690620fe
Bhavana V <bhavsv500401@gmail.com>
2026-02-20 15:04:43 +0530

    day2

---
+++ cal.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+  public class cal{
+    public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter the num1:");
+      double num1=sc.nextDouble();
+      System.out.println("enter the num2:");
+      double num2=sc.nextDouble();
+      System.out.println("enter the operation(+,*,/,-):");
+      char op =sc.next().charAt(0);
+      switch(op){
+        case '+':
+          System.out.println("result:"+(num1+num2));
+          break;
+        case '-':
+          System.out.println("result:"+(num1-num2));
+          break;
+        case '*':
+          System.out.println("result:"+(num1*num2));
+          break;  
+        case '/':
+          if(num2 !=0)
+            System.out.println("result:"+(num1/num2));
+        else
+          System.out.println("error");
+        break;
+        default:
+          System.out.println("invalid operator");
+      }
+      sc.close();
+    }
+  }
+
+
