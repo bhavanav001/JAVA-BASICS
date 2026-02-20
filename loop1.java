7cf134ecb51bca2c927d272d552231a06fc9aea4
Bhavana V <bhavsv500401@gmail.com>
2026-02-20 13:26:43 +0530

    day2

---
+++ loop1.java
@@ -0,0 +1,18 @@
+public class loop1 {
+  public static void main(String[] args) {
+    for(int i=1;i<=5;i++){
+      if(i>3){
+        break;
+      }
+      System.out.print(i);
+    }
+    for(int i=1;i<=5;i++){
+      if(i==3){
+        continue;
+      }
+      System.out.println(i);
+
+    }
+  }
+  
+}
