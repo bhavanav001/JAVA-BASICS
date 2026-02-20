22cdd56d03031da42615c1173c320d74edee58c9
Bhavana V <bhavsv500401@gmail.com>
2026-02-20 13:11:47 +0530

    day2

---
+++ wrapping.java
@@ -0,0 +1,10 @@
+public class wrapping {
+  public static void main(String[] args) {
+    int n = 10;
+    Integer obj=n;//autoboxing
+    int b=obj;
+    System.out.println(obj);
+    System.out.println(b);//unboxing
+  }
+  
+}
